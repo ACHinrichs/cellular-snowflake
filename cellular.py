993ecfb39427f3c789a95f43cba0c6335fd5abeb
Adrian C. Hinrichs <adrian.hinrichs@rwth-aachen.de>
2017-12-23 18:01:39 +0100

    Fix cellular

--- cellular.py
+++ cellular.py
@@ -7,48 +7,53 @@
 from abc import ABC, abstractmethod, abstractproperty
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
 import time
 
-class Rule(ABC):
+class Ruler(ABC):
     @abstractmethod
     def __init__(self):
-        self.cmap = colors.ListedColormap(['white', 'blue',])
+        self.cmap = colors.ListedColormap(['white', 'black',])
         pass
     
     @abstractmethod
-    def rule(x,y,field,newfield):
-        pass
+    def rule(self,x,y,field,newfield):
+        newField=field
     
     @abstractmethod
-    def initField():
-        pass
+    def initField(self):
+        return array([])
 
 
 # The cellualar automaton itself
-class Cellular:
-    def __init__(self, rule):
-        self.ruler = rule
+class Automaton:
+    delay = 0.25 
+    def __init__(self, ruler):
+        self.ruler = ruler
         self.field=ruler.initField()
-        paint(field)
         self.fig = plt.figure()
 
         frame = plt.gca()
         frame.axes.get_xaxis().set_visible(False)
         frame.axes.get_yaxis().set_visible(False)
-        self.grid = plt.imshow(field,
+        self.grid = plt.imshow(self.field,
                                interpolation='nearest',
                                cmap=self.ruler.cmap)
+        plt.ion() 
         plt.show()
+        self.paint(self.field)
         
-    def paint(field):
-        grid.set_data(new_state)
+    def paint(self,field):
+        self.grid.set_data(field)
+        self.fig.canvas.draw()
         
     def start(self):
-        running = true
+        running = True
         while(running):
-            time.sleep(1)
-            newFieled= np.zeros(field.shape)
-            for x,y in numpy.nditer(field):
-                ruler.rule(x,y,field,newField)
-            field=newField
-            paint(field)
+            time.sleep(self.delay)
+            newField= np.zeros(self.field.shape)
+            for x,row in enumerate(self.field):
+                for y,cell in enumerate(row):
+                    self.field,newField=self.ruler.rule(x,y,self.field, newField)
+            self.field=newField
+            self.paint(self.field)
